e496440c2cff0c682a3c8721bf8c038189ce3eb5
will <818@holbertonschool.com>
2019-12-10 15:40:54 +0000

    [INIT] Do first try

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# This disables password login and uses id file of ~/.ssh/holberton
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
